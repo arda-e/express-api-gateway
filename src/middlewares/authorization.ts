8d1927460dfe6288aed4710c519e615f3508322b
AE <aeren@netas.com.tr>
2024-11-07 21:28:15 +0300

    Add authorization middleware for permission checks
    Introduced a middleware to verify if the user has the necessary permissions for accessing specific resources. The middleware throws an error if the user is not authenticated or lacks the required permissions, improving the security of the application.

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,41 @@
+import { Request, Response, NextFunction, RequestHandler } from 'express';
+import { AuthenticationError, AuthorizationError } from '@utils/errors';
+import { container } from 'tsyringe';
+import PermissionService from '@api/v1/permission/permission.service';
+
+interface RequestWithUser extends Request {
+  user?: {
+    id: string;
+  };
+}
+/**
+ * Middleware function that checks if the current user has the required permissions to access a resource.
+ *
+ * @param {string[]} requiredPermissions - An array of permission names that the user must have to access the resource.
+ * @returns {RequestHandler} - An Express middleware function that performs the authorization check.
+ */
+export const authorization = (requiredPermissions: string[]): RequestHandler => {
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const userId = (req as RequestWithUser).user?.id;
+      if (!userId) {
+        throw new AuthenticationError('User not authenticated');
+      }
+
+      const permissionService = container.resolve(PermissionService);
+      const userPermissions = await permissionService.getPermissionByUserId(userId);
+
+      const hasRequiredPermissions = requiredPermissions.every((requiredPermission) =>
+        userPermissions.some((userPermission) => userPermission.name === requiredPermission),
+      );
+
+      if (!hasRequiredPermissions) {
+        throw new AuthorizationError('User does not have required permissions');
+      }
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  };
+};
