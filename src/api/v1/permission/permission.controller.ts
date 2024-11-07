37fa45d93ef45a6fded11515893d13974ca96b67
AE <aeren@netas.com.tr>
2024-11-07 21:26:19 +0300

    feat(controllers): create role and permission controllers for request handling
    - Implemented controllers for managing role and permission requests.
    - Integrated response builders for consistent API responses.

---
+++ src/api/v1/permission/permission.controller.ts
@@ -0,0 +1,125 @@
+import { RequestHandler } from 'express';
+import { container } from 'tsyringe';
+import { StatusCodes } from 'http-status-codes';
+import { ResourceDoesNotExistError, ResourceAlreadyExistsError } from '@utils/errors';
+import { ErrorResponseBuilder, ResponseBuilder } from '@utils/ResponseBuilder';
+
+import PermissionService from './permission.service';
+import Permission from './permission.model';
+
+const permissionService = container.resolve(PermissionService);
+
+export const getPermissions: RequestHandler = async (req, res, next) => {
+  try {
+    const permissions = await permissionService.getPermissions();
+    res
+      .status(StatusCodes.OK)
+      .json(
+        new ResponseBuilder()
+          .setStatus('success')
+          .setStatusCode(StatusCodes.OK)
+          .setMessage('Permissions retrieved successfully')
+          .setData(permissions)
+          .build(),
+      );
+  } catch (error) {
+    if (error instanceof ResourceDoesNotExistError) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json(new ErrorResponseBuilder(StatusCodes.NOT_FOUND, error.message).build());
+    } else {
+      next(error);
+    }
+  }
+};
+
+export const getPermission: RequestHandler = async (req, res, next) => {
+  try {
+    const permission = await permissionService.getPermission(req.params.id);
+    res
+      .status(StatusCodes.OK)
+      .json(
+        new ResponseBuilder()
+          .setStatus('success')
+          .setStatusCode(StatusCodes.OK)
+          .setMessage('Permission retrieved successfully')
+          .setData(permission)
+          .build(),
+      );
+  } catch (error) {
+    if (error instanceof ResourceDoesNotExistError) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json(new ErrorResponseBuilder(StatusCodes.NOT_FOUND, error.message).build());
+    } else {
+      next(error);
+    }
+  }
+};
+
+export const createPermission: RequestHandler = async (req, res, next) => {
+  try {
+    const newPermission = new Permission(undefined, req.body.name, req.body.description);
+    const createdPermission = await permissionService.createPermission(newPermission);
+
+    res
+      .status(StatusCodes.CREATED)
+      .json(
+        new ResponseBuilder()
+          .setStatus('success')
+          .setStatusCode(StatusCodes.CREATED)
+          .setMessage('Permission created successfully')
+          .setData(createdPermission)
+          .build(),
+      );
+  } catch (error) {
+    if (error instanceof ResourceAlreadyExistsError) {
+      res
+        .status(StatusCodes.CONFLICT)
+        .json(new ErrorResponseBuilder(StatusCodes.CONFLICT, error.message).build());
+    } else {
+      next(error);
+    }
+  }
+};
+
+export const updatePermission: RequestHandler = async (req, res, next) => {
+  try {
+    const updatedPermission = new Permission(req.params.id, req.body.name, req.body.description);
+    const result = await permissionService.updatePermission(req.params.id, updatedPermission);
+
+    res
+      .status(StatusCodes.OK)
+      .json(
+        new ResponseBuilder()
+          .setStatus('success')
+          .setStatusCode(StatusCodes.OK)
+          .setMessage('Permission updated successfully')
+          .setData(result)
+          .build(),
+      );
+  } catch (error) {
+    if (error instanceof ResourceDoesNotExistError) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json(new ErrorResponseBuilder(StatusCodes.NOT_FOUND, error.message).build());
+    } else {
+      next(error);
+    }
+  }
+};
+
+export const deletePermission: RequestHandler = async (req, res, next) => {
+  try {
+    await permissionService.deletePermission(req.params.id);
+    res.status(StatusCodes.NO_CONTENT).send();
+  } catch (error) {
+    if (error instanceof ResourceDoesNotExistError) {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json(new ErrorResponseBuilder(StatusCodes.NOT_FOUND, error.message).build());
+    } else {
+      next(error);
+    }
+  }
+};
---
+++ src/api/v1/role/role.controllers.ts
@@ -0,0 +1,221 @@
+import { Request, Response, NextFunction } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { container } from 'tsyringe';
+import { ResponseBuilder } from '@utils/ResponseBuilder';
+
+import RoleService from './role.service';
+
+const roleService = container.resolve(RoleService);
+export const getRoles = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const roles = await roleService.getRoles();
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setData(roles)
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+export const createRole = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    // !TODO: check service for correct error throw
+    const role = await roleService.createRole(req.body);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.CREATED)
+      .setMessage('Role successfully created.')
+      .setStatus('success')
+      .setData(role)
+      .build();
+
+    res.status(StatusCodes.CREATED).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+export const getRole = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const roleId = req.params.id;
+    // !TODO: check service for correct error throw
+    const role = await roleService.getRole(roleId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setData(role)
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+export const updateRole = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const roleId = req.params.id;
+    const role = await roleService.updateRole(roleId, req.body);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Role successfully updated.')
+      .setData(role)
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+export const deleteRole = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const roleId = req.params.id;
+    await roleService.deleteRole(roleId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Role successfully deleted.')
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+export const assignRoleToUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { userId, roleId } = req.body;
+    // !TODO: check service for correct error throw
+    const updatedUser = await roleService.assignRoleToUser(userId, roleId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Role successfully assigned to user.')
+      .setData(updatedUser)
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removeRoleFromUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { userId, roleId } = req.body;
+    // !TODO: check service for correct error throw
+    await roleService.removeRoleFromUser(userId, roleId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Role successfully removed from user.')
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserRoles = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    // !TODO: check service for correct error throw
+    const roles = await roleService.getUserRoles(userId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setData(roles)
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const assignPermissionToRole = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { roleId, permissionId } = req.body;
+    await roleService.assignPermissionToRole(roleId, permissionId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Permission successfully assigned to role.')
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const removePermissionFromRole = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { roleId, permissionId } = req.body;
+    await roleService.removePermissionFromRole(roleId, permissionId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Permission successfully removed from role.')
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getRolePermissions = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const roleId = req.params.roleId;
+    const permissions = await roleService.getRolePermissions(roleId);
+    const response = new ResponseBuilder()
+      .setStatusCode(StatusCodes.OK)
+      .setStatus('success')
+      .setMessage('Permissions successfully retrieved.')
+      .setData(permissions)
+      .build();
+
+    res.status(StatusCodes.OK).json(response);
+  } catch (error) {
+    next(error);
+  }
+};
