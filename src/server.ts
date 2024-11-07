43e02f2c4b2b8a3bbce467761b85eb7bfd879b12
AE <aeren@netas.com.tr>
2024-11-07 21:04:01 +0300

    feat(server): implement graceful shutdown with enhanced error handling and dependency management
    - Added event listeners for SIGTERM, SIGINT, and uncaught exceptions to ensure smooth server shutdown.
    - Refactored `gracefulShutdown` function to handle server, session, and database closure within a race condition to meet shutdown timeout.
    - Integrated `tsyringe` for dependency injection to simplify session configuration and database manager handling.
    - Enhanced error logging for both server start and shutdown processes.
    - Ensured structured handling of uncaught exceptions to maintain application stability.

--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,19 @@ import LoggerFactory from '@utils/Logger';
 import { initializeDependencies } from '@config/dependencies';
 import SessionConfig from '@config/sessionConfig';
 import Config from '@config/config';
+import { container } from 'tsyringe';
 
-import DbFactory from './db/db.factory';
-import DatabaseFactory from './db/db.factory';
+import DatabaseManager from './db/db.manager';
 
 const logger = LoggerFactory.getLogger();
+const SERVER_PORT = Config.server.port;
+const SHUTDOWN_TIMEOUT = Config.server.shutdownTimeout;
+
+enum SHUTDOWN_EVENTS {
+  SIGINT = 'SIGINT',
+  SIGTERM = 'SIGTERM',
+  UNCAUGHT_EXCEPTION = 'uncaughtException',
+}
 
 /**
  * Starts the server and sets up event listeners for graceful shutdown and uncaught exceptions.
@@ -19,27 +27,51 @@ const logger = LoggerFactory.getLogger();
  *
  * @returns A Promise that resolves when the server is started and running.
  */
-const startServer = async (): Promise<void> => {
+const startServer = async (
+  sessionConfig: SessionConfig = container.resolve(SessionConfig),
+  databaseFactory: DatabaseManager = container.resolve(DatabaseManager),
+): Promise<Server> => {
   try {
-    await initializeDependencies(SessionConfig, DatabaseFactory);
-
+    await initializeDependencies(sessionConfig, databaseFactory);
     const { default: app } = await import('./app');
 
-    const server: Server = app.listen(Config.server.port, () => {
-      logger.info(`API Gateway running on port ${Config.server.port}`);
+    const server: Server = app.listen(SERVER_PORT, () => {
+      logger.info(`API Gateway running on port ${SERVER_PORT}`);
     });
 
-    process.on('SIGTERM', () => gracefulShutdown(SessionConfig, DatabaseFactory, server));
-    process.on('SIGINT', () => gracefulShutdown(SessionConfig, DatabaseFactory, server));
-    process.on('uncaughtException', (err) => {
-      logger.error(`Uncaught exception: ${err.message}`);
-      process.exit(1);
-    });
-  } catch (error) {
-    logger.error(`Failed to start server: ${error.message}`);
+    setupEventListeners(server);
+
+    return server;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(`Failed to start server: ${error.message}`);
+    } else {
+      logger.error(`Failed to start server: ${String(error)}`);
+    }
     process.exit(1);
   }
 };
+
+/**
+ * Sets up event listeners for SIGTERM, SIGINT, and uncaught exceptions.
+ *
+ * @param server - The HTTP server instance.
+ */
+function setupEventListeners(server: Server): void {
+  process.on(SHUTDOWN_EVENTS.SIGINT, () => gracefulShutdown(server));
+  process.on(SHUTDOWN_EVENTS.SIGTERM, () => gracefulShutdown(server));
+  process.on(SHUTDOWN_EVENTS.UNCAUGHT_EXCEPTION, handleUncaughtException);
+}
+
+/**
+ * Handles uncaught exceptions by logging the error and exiting the process.
+ *
+ * @param error - The error that was thrown.
+ */
+function handleUncaughtException(error: Error): void {
+  logger.error(`Uncaught exception: ${error.message}`);
+  process.exit(1);
+}
 
 /**
  * Gracefully shuts down the server and associated resources.
@@ -48,41 +80,56 @@ const startServer = async (): Promise<void> => {
  * configuration, and closing the database connection. It uses a race condition
  * to ensure the shutdown completes within a configured timeout.
  *
- * @param SessionConfig - The session configuration module.
- * @param DatabaseFactory - The database factory module.
  * @param server - The HTTP server instance to be closed.
  * @returns A Promise that resolves when the shutdown is complete.
  */
-async function gracefulShutdown(
-  SessionConfig: typeof SessionConfig,
-  DatabaseFactory: typeof DatabaseFactory,
-  server: Server,
-): Promise<void> {
+export async function gracefulShutdown(server: Server): Promise<void> {
   logger.info('Graceful shutdown initiated');
-  async function handleServerClose(): Promise<void> {
-    return new Promise<void>((resolve) => {
-      server.close(async () => {
-        await new SessionConfig.destroyInstance();
-        const db = DbFactory.getDatabase();
-        await db.close();
-        resolve();
-      });
-    });
-  }
-
   try {
     // Wait for either the server to close or the timeout to occur.
     await Promise.race([
-      handleServerClose(),
+      handleServerClose(server),
       new Promise<void>((_, reject) =>
-        setTimeout(() => reject(new Error('Shutdown timeout')), Config.server.shutdownTimeout),
+        setTimeout(() => reject(new Error('Shutdown timeout')), SHUTDOWN_TIMEOUT),
       ),
     ]);
     logger.info('Server closed successfully');
-  } catch (error) {
-    logger.error(`Error during graceful shutdown: ${error.message}`);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      logger.error(`Error during graceful shutdown: ${error.message}`);
+    } else {
+      logger.error('Error during graceful shutdown: Unknown error');
+    }
   } finally {
     process.exit(0);
+  }
+}
+
+export async function handleServerClose(
+  server: Server,
+  sessionConfig: SessionConfig = container.resolve(SessionConfig),
+  databaseManager: DatabaseManager = container.resolve(DatabaseManager),
+): Promise<void> {
+  try {
+    await new Promise<void>((resolve, reject) => {
+      server.close((error) => {
+        if (error) {
+          logger.error('Error closing the server:', error);
+          return reject(error);
+        }
+        logger.info('Server closed successfully.');
+        resolve();
+      });
+    });
+
+    await sessionConfig.destroy();
+    logger.info('Session configuration destroyed.');
+
+    // Close the database connection
+    await databaseManager.close();
+    logger.info('Database connection closed.');
+  } catch (error) {
+    logger.error('Error during graceful shutdown:', error);
   }
 }
 
